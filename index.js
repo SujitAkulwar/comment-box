92ae3f747c29dd5a9fc2b5161f7ebdabe5976123
Sujit Akulwar <sujitakulwar@gmail.com>
2022-05-30 22:37:53 -0700

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+var post= document.getElementById("post");
+
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment").value;
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered").appendChild(li);
+});
